c41af87ff8926c4096523651137af2020de81add
Harshuv9 <harshitpatelv9@gmail.com>
2023-03-31 00:20:28 +0530

    .

---
+++ TASKS/Extra Tasks/leap year.py
@@ -0,0 +1,7 @@
+year=int(input("Enter year :"))
+print("The Rmtered Year is : ",year)
+
+if year%4==0:
+    print(" Year Is leap year")
+else:
+    print(" Year Is Not Leap Year")
---
+++ TASKS/Extra Tasks/loops-2.py
@@ -0,0 +1,6 @@
+number=int(input("Enter Number : "))
+if number%5==0 and number%5==0:
+     print(f"{number} is divided with 5")
+
+else :
+     print(f"{number} is divided with 11")
